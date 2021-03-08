ce931936a887612e1ea03a7b3248d2490dbabb5d
Danielle Thompson <danithompson74@gmail.com>
2021-03-08 13:06:56 -0800

    Center align spotify player

--- tiki3-0/src/page-components/spotify-player/spotify-player.js
+++ tiki3-0/src/page-components/spotify-player/spotify-player.js
@@ -3,7 +3,9 @@ import React from 'React';
 function SpotifyPlayer() {
   return (
     <React.Fragment>
-      <iframe src="https://open.spotify.com/embed/playlist/5wiP5B51Bn9Q9Fb3HwUbJ0" width="300" height="80" frameBorder="0" allowtransparency="true" allow="encrypted-media"></iframe>
+      <div align="center" style={{paddingBottom: '30px'}}>
+        <iframe src="https://open.spotify.com/embed/playlist/5wiP5B51Bn9Q9Fb3HwUbJ0" width="300" height="80" frameBorder="0" allowtransparency="true" allow="encrypted-media"></iframe>
+      </div>
     </React.Fragment>
   )
 }
