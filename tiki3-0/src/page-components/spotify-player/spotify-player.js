69da1745b40a11bc2d61c77fc1f5834ebc1b4c5f
Danielle Thompson <danithompson74@gmail.com>
2021-03-09 20:58:31 -0800

    Tidy

--- tiki3-0/src/page-components/spotify-player/spotify-player.js
+++ tiki3-0/src/page-components/spotify-player/spotify-player.js
@@ -3,11 +3,18 @@ import React from 'React';
 function SpotifyPlayer() {
   return (
     <React.Fragment>
-      <div align="center" style={{paddingBottom: '30px'}}>
-        <iframe src="https://open.spotify.com/embed/playlist/5wiP5B51Bn9Q9Fb3HwUbJ0" width="300" height="80" frameBorder="0" allowtransparency="true" allow="encrypted-media"></iframe>
+      <div align="center" style={{ paddingBottom: '30px' }}>
+        <iframe
+          src="https://open.spotify.com/embed/playlist/5wiP5B51Bn9Q9Fb3HwUbJ0"
+          width="300"
+          height="80"
+          frameBorder="0"
+          allowtransparency="true"
+          allow="encrypted-media"
+        ></iframe>
       </div>
     </React.Fragment>
-  )
+  );
 }
 
 export default SpotifyPlayer;
